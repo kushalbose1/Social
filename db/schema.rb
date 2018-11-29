72c423641890d407e13b96ffaaf7b802bb4ea821
kushalbose1 <k.bose@grepruby.com>
2018-11-29 10:58:50 +0530

    Friendship model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_052313) do
+ActiveRecord::Schema.define(version: 2018_11_29_052510) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 2018_11_29_052313) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
