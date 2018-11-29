dc51dd2dddb54d8c96c8db7e578f5d0af3d187b7
kushalbose1 <k.bose@grepruby.com>
2018-11-29 12:16:19 +0530

    image model and migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments
+  has_many :images
+  has_many :likes,as: :likeable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_052510) do
+ActiveRecord::Schema.define(version: 2018_11_29_064545) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 2018_11_29_052510) do
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
